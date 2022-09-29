518a69e654eb6a68801706ffff6aaaf4c16a9f4c
Arfanul Hoque Erfan <mderfan3928@gmail.com>
2022-09-29 08:19:31 +0600

    added count style

--- src/components/Count/Count.js
+++ src/components/Count/Count.js
@@ -1,62 +1,81 @@
-import React from 'react';
-import './Count.css'
-import profile from '../../images/20211007_075745.jpg'
+import React from "react";
+import "./Count.css";
+import profile from "../../images/20211007_075745.jpg";
 
-const Count = () => {
-    return (
-        <div className='profile-content'>
-           <div className='dashboard'> 
-<div className="user-info"> 
-<div className='user'> 
-<img src={profile} alt="" /> 
-<div> 
-<h4>Md. Erfan</h4> 
-<p>Dhaka, Bangladesh</p> 
-</div> 
-</div> 
-<div className='user-details'> 
-<div> 
-<h4><b>75</b>kg</h4> 
-<p>Weight</p> 
-</div> 
-<div> 
-<h4><b>5.7</b></h4> 
-<p>Height</p> 
-</div> 
-<div> 
-<h4><b>21</b></h4> 
-<p>Years</p> 
-</div> 
-</div> 
-</div> 
-<div className='break-div'> 
-<h5>Add A Break</h5> 
-<div className='break'> 
-<p><span>10</span>s</p> 
-<p ><span>20</span>s</p> 
-<p ><span>30</span>s</p> 
-<p ><span>40</span>s</p> 
-<p ><span>50</span>s</p> 
-</div> 
-</div> 
-<div className='exercise-details'> 
-<h5>Exercise Details</h5> 
-<div className="excercise-box"> 
-<div className='exercise-time'> 
-<p>Exercise time</p> 
-<p><span>0</span> secound</p> 
-</div> 
-<div className='break-time'> 
-<p>Break time</p> 
-<p><span>0</span> secound</p> 
-</div> 
-</div> 
-<button>Activity Completed</button> 
-</div> 
-</div>
-
+const Count = ({ count }) => {
+  return (
+    <div className="count-content">
+      <div className="dashboard">
+        <div className="user-info">
+          <div className="user">
+            <img src={profile} alt="" />
+            <div>
+              <h4>Md. Erfan</h4>
+              <p>Dhaka, Bangladesh</p>
+            </div>
+          </div>
+          <div className="user-details">
+            <div>
+              <h4>
+                <b>75</b>kg
+              </h4>
+              <p>Weight</p>
+            </div>
+            <div>
+              <h4>
+                <b>5.7</b>
+              </h4>
+              <p>Height</p>
+            </div>
+            <div>
+              <h4>
+                <b>21</b>
+              </h4>
+              <p>Years</p>
+            </div>
+          </div>
         </div>
-    );
+        <div className="break-div">
+          <h5>Add A Break</h5>
+          <div className="break">
+            <p>
+              <span>10</span>s
+            </p>
+            <p>
+              <span>20</span>s
+            </p>
+            <p>
+              <span>30</span>s
+            </p>
+            <p>
+              <span>40</span>s
+            </p>
+            <p>
+              <span>50</span>s
+            </p>
+          </div>
+        </div>
+        <div className="exercise-details">
+          <h5>Exercise Details</h5>
+          <div className="excercise-box">
+            <div className="exercise-time">
+              <p>Exercise time</p>
+              <p>
+                <span>0</span> secound
+              </p>
+            </div>
+            <div className="break-time">
+              <p>Break time</p>
+              <p>
+                <span>0</span> secound
+              </p>
+            </div>
+          </div>
+          <button>Activity Completed</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Count;
--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,44 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import Activities from '../Activities/Activities';
-import Count from '../Count/Count';
-import './Exercise.css'
+import React, { useEffect, useState } from "react";
+import Activities from "../Activities/Activities";
+import Count from "../Count/Count";
+import "./Exercise.css";
 
 const Exercise = () => {
+  const [exercises, setExercises] = useState([]);
+  const [count, setCount] = useState([]);
 
-     const [exercises, setExercises] = useState([]);
-     const [count, setCount] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setExercises(data));
+  }, []);
 
-    useEffect(() =>{
-        fetch('data.json')
-        .then(res => res.json())
-        .then(data => setExercises(data))
-    }, [])
+  const handleAddToCount = (exercise) => {
+    console.log(exercise);
+    const newCount = [...count, exercise];
+    setCount(newCount);
+  };
 
-    const handleAddToCount = (exercise) =>{
-        console.log(exercise);
-        const newCount =[...count, exercise]
-        setCount(newCount)
-    }
-
-    return (
-        <div>
-            <h3>Select Today's Exercise</h3>
-            <div className='exercise-container'>
-            <div className="exercise-formations">
-             {
-                 exercises.map(exercise=> <Activities
-                 key={exercise.id}
-                 exercise={exercise}
-                 handleAddToCount={handleAddToCount}
-                 ></Activities>)
-             }
-            </div>
-            <div className="count-container">
-              <Count></Count>
-            </div>
-            </div>
+  return (
+    <div>
+      <div className="exercise-container">
+        <div className="exercise-formations">
+          {exercises.map((exercise) => (
+            <Activities
+              key={exercise.id}
+              exercise={exercise}
+              handleAddToCount={handleAddToCount}
+            ></Activities>
+          ))}
         </div>
-    );
+        <div className="count-container">
+          <Count count={count}></Count>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Exercise;
